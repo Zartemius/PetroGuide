e7e7583749b51d36b10a9166077c07b30aed6ede
Artem <dartemion@gmail.com>
2018-12-19 22:54:18 +0300

    creation of menuActivity was started

---
+++ app/src/main/java/com/example/darte/petroguide/MenuActivity.java
@@ -0,0 +1,19 @@
+package com.example.darte.petroguide;
+
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void createMenu(){
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle()
+    }
+
+}
---
+++ app/src/main/java/com/example/darte/petroguide/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.example.darte.petroguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+    }
+}
